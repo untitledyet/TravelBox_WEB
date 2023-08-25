65b82f157416059e59e24a145df308ff39bfd200
untitledyet <53141248+untitledyet@users.noreply.github.com>
2023-08-25 21:25:50 +0400

    travelbox1

--- app.py
+++ app.py
@@ -1,65 +1,36 @@
-import random
-import sms_validation
-import datetime
-
-from flask import Flask, render_template, request
+from flask import Flask, request
 import psycopg2
-import smsoffice
+from psycopg2 import sql
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    link = request.form['link']
-    phone = "+995" + request.form['phone']
-    expiration_day = request.form['expiration_day']
-    print(expiration_day)
-    code = request.form['sms_code']
-
-    current_date = datetime.datetime.now().date()
-    print(current_date)
-    future_date = current_date + datetime.timedelta(days=(int(expiration_day) + 1))
-    print(future_date)
-    expiration_date = future_date.strftime('%Y-%m-%d')
-
-    conn = psycopg2.connect(database="MyAuto",
+def save_code_to_db(code, table_name, column_name):
+    conn = psycopg2.connect(database="travelbox_db",
                             user="doadmin",
-                            password="AVNS_ccDIJkyd8h2KOE23NtN",
-                            host="db-postgresql-fra1-89602-do-user-13590454-0.b.db.ondigitalocean.com",
+                            password="AVNS_i-7e1eOujhl8LWLX-yu",
+                            host="travelbox-do-user-14253771-0.b.db.ondigitalocean.com",
                             port="25060")
+    cursor = conn.cursor()
 
-    if phone == sms_validation.phone_number and int(code) == int(sms_validation.sms_code):
-        cur = conn.cursor()
-        cur.execute(
-            "INSERT INTO links_table (link, phone, expiration_date) VALUES (%s, %s, %s)",
-            (
-                link, phone, expiration_date))
-        conn.commit()
-        cur.close()
-        conn.close()
-        return "Your form was submitted successfully!"
-    else:
-        return "Verification Code is Wrong!"
+    insert_query = sql.SQL("INSERT INTO {} ({}) VALUES (%s);").format(
+        sql.Identifier(table_name),
+        sql.Identifier(column_name)
+    )
+    cursor.execute(insert_query, [code])
 
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-@app.route('/send_sms_code', methods=['POST'])
-def send_sms_code():
-    phone = request.json.get('phone')
-    code = random.randrange(1000, 9999)
+@app.route('/bogpay_authorization/<code>', methods=['GET'])
+def handle_authorization(code):
+    save_code_to_db(code, "bogpay_codes", "temp_codes")
+    return "Code saved to the 'bogpay_codes' table."
 
-    sms_validation.phone_number = "+995" + str(phone)
-    sms_validation.sms_code = code
-
-    smsoffice.smsoffice_send(phone, code)
-    print('SMS code sent to', phone)
-    return 'SMS code sent to {}'.format(phone)
-
+@app.route('/bog_payment/<code>', methods=['GET'])
+def handle_payment(code):
+    save_code_to_db(code, "bog_payment", "pay_codes")
+    return "Code saved to the 'payment_codes' table."
 
 if __name__ == '__main__':
     app.run(debug=True)
